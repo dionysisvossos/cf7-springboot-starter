b4623eaa08a3d74d860fab1cd34ab584d027166b
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-05-22 20:17:03 +0300

    feat: application controller created

---
+++ src/main/java/gr/aueb/cf/helloapp/controller/HelloController.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.helloapp.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/coding")
+    public String sayHello(Model model) {
+        model.addAttribute("message", "Hello All!");
+        return "index";
+    }
+
+    @GetMapping("/welcome")
+    public String sayWelcome(@RequestParam(defaultValue = "Guest") String name, Model model) {
+        model.addAttribute("name", name);
+        return "welcome";
+    }
+}
